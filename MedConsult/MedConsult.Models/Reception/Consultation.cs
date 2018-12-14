2359fdfcbd55ec36288a0172bfaca8b2f891121c
Karina Wittal <karinishmandarinish@gmail.com>
2018-12-14 18:09:36 +0200

    add changes

---
+++ MedConsult/MedConsult.Models/Reception/Consultation.cs
@@ -0,0 +1,22 @@
+﻿using MedConsult.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedConsult.Models.Reception
+{
+    public class Consultation : Entity
+    {
+        public DateTime DateOfConsultation { get; set; }
+        public DateTime TimeFrom { get; set; }
+        public DateTime TimeTo { get; set; }
+
+        public Guid Doctor { get; set; }
+        public bool IsAccepted { get; set; }
+        public Guid Patient { get; set; }
+        public int Duration { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+}
